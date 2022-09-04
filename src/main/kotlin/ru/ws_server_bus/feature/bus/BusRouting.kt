b3f2e86efc8a7227574229f91a59d0711a3f38c1
sergey.usachev <sergey.usachev@vk.team>
2022-09-04 23:29:10 +0300

    Create socket with multiply connections

--- src/main/kotlin/ru/ws_server_bus/feature/bus/BusRouting.kt
+++ src/main/kotlin/ru/ws_server_bus/feature/bus/BusRouting.kt
@@ -4,8 +4,12 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import java.util.Collections
+import java.util.LinkedHashSet
+import java.util.concurrent.atomic.AtomicInteger
 
 fun Application.configureBusRouting() {
+
     routing {
         webSocket("/echo") {
             send("Please enter your name")
@@ -25,4 +29,30 @@ fun Application.configureBusRouting() {
             }
         }
     }
+
+    routing {
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+        webSocket("/echoN") {
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            send("You've logged in as [${thisConnection.name}]")
+
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                connections.forEach {
+                    it.session.send(textWithUsername)
+                }
+            }
+        }
+    }
+}
+
+class Connection(val session: DefaultWebSocketServerSession) {
+    companion object {
+        val lastId = AtomicInteger(0)
+    }
+
+    val name = "user${lastId.getAndIncrement()}"
 }
